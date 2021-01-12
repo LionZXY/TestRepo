22b2754ded72abb8f07860ca357e040e3d93b9bb
LionZXY <nikita@kulikof.ru>
2021-01-12 12:52:27 +0200

    Update send_message.py

--- .github/actions/tg/send_message.py
+++ .github/actions/tg/send_message.py
@@ -1,13 +1,16 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
+import json
 import os
 
 url = 'https://api.telegram.org/bot'+ os.environ['TG_TOKEN'] + '/sendMessage'
 payload = {'chat_id': os.environ['CHAT_ID'], 'parse_mode': 'markdown', 'disable_web_page_preview': True, 'text': os.environ['TEXT']} 
+json_payload =  json.dumps(payload).encode('utf8')
 
-print(payload)
+print(json_payload)
 print(url)
 
-request = Request(url, urlencode(payload).encode())
-response = urlopen(request).read()
+request = Request(url, data=json_payload,
+                 headers={'content-type': 'application/json'})
+response = urlopen(request).read().decode('utf8')
 print(response)
