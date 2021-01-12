b2a68042fa1a131168b0513452a3a61e51cf9927
LionZXY <nikita@kulikof.ru>
2021-01-12 13:20:37 +0200

    Update send_message.py

--- .github/actions/tg/send_message.py
+++ .github/actions/tg/send_message.py
@@ -3,7 +3,7 @@ import json
 import os
 
 
-text = os.environ['CHAT_ID']
+text = os.environ['TEXT']
 print(text)
 payload = {'chat_id': os.environ['CHAT_ID'], 'parse_mode': 'markdown', 'disable_web_page_preview': True, 'text': str(text)} 
 json_payload =  json.dumps(payload).encode('utf8')
