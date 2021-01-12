4a36de35171b0c6b89eb132d540cfae5d13efc26
LionZXY <nikita@kulikof.ru>
2021-01-12 13:14:27 +0200

    Update send_message.py

--- .github/actions/tg/send_message.py
+++ .github/actions/tg/send_message.py
@@ -2,12 +2,10 @@ import http.client
 import json
 import os
 
-url = 'https://enlesz7htgigo.x.pipedream.net/bot'+ os.environ['TG_TOKEN'] + '/sendMessage'
-payload = {'chat_id': os.environ['CHAT_ID'], 'parse_mode': 'markdown', 'disable_web_page_preview': True, 'text': os.environ['TEXT']} 
+payload = {'chat_id': os.environ['CHAT_ID'], 'parse_mode': 'markdown', 'disable_web_page_preview': True, 'text': 'Test'} 
 json_payload =  json.dumps(payload).encode('utf8')
 
 print(json_payload)
-print(url)
 
 conn = http.client.HTTPSConnection('api.telegram.org')
 conn.request("POST", '/bot'+ os.environ['TG_TOKEN'] + '/sendMessage', json_payload, {'Content-Type': 'application/json'})
